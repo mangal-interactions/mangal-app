2b14374534a0a26a4824b3232005eebf686e3e84
Steve Vissault <s.vissault@yahoo.fr>
2019-03-10 23:09:55 -0400

    Add n network attached; and start exportBib fun

--- src/components/Dataset.vue
+++ src/components/Dataset.vue
@@ -11,23 +11,25 @@
           <span class="font-weight-bold body-2">Dataset name:</span> {{ getDataset.name }}
         </div>
         <div>
+          <span class="font-weight-bold body-2">Number of networks attached:</span> {{ getDataset.networks.length }}
+        </div>
+        <div>
           <span class="font-weight-bold body-2">Description:</span> {{ getDataset.description }}
         </div>
         <div><span class="font-weight-bold body-2">Status:</span>
-          <v-chip small v-if="getDataset.public" color="primary" text-color="white">Public</v-chip>
+          <v-chip small v-if="getDataset.public" color="primary" text-color="white" style="font-size:11px;margin:0px;">Public</v-chip>
           <v-chip small v-else color="orange" text-color="white">Private</v-chip>
         </div>
         <div><span class="font-weight-bold body-2">Last update:</span>
           {{ formatUpdate }}
         </div>
-        <div>
-          <span class="font-weight-bold body-2">Number of networks attached:</span> Coming soon
-        </div>
       </div>
       <div class="title pt-4 teal--text">Citation</div>
       <v-divider class="pb-2"></v-divider>
       <v-flex pt-3 x12>
-        <div v-if="getRef" class="text-md-left">{{ getCitation }}</div>
+        <div v-if="getRef" class="text-md-left">{{ getCitation }}<br>
+        <v-btn @click="exportBib" small depressed outline color="teal" text-color="white" style="font-size:11px;margin:0px;text-transform: none !important;">Export as BibTex</v-btn>
+        </div>
       </v-flex>
       <div class="title pt-4 teal--text">Sources</div>
       <v-divider class="pb-2"></v-divider>
@@ -46,8 +48,15 @@
 <script>
 import { mapGetters } from 'vuex'
 import Cite from 'citation-js'
+import { saveAs } from 'file-saver'
 import moment from 'moment'
 export default {
+  method: {
+    exportBib: function () {
+      var blob = new Blob(this.getRef.bibtex, {type: 'text/plain;charset=utf-8'})
+      saveAs(blob, 'bibtex.txt')
+    }
+  },
   computed: {
     ...mapGetters([
       'getRef',
