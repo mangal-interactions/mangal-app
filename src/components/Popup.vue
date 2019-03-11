110ed0607a4f5959311132553a6dc977ef46b88b
Steve Vissault <s.vissault@yahoo.fr>
2019-03-11 02:14:09 -0400

    Minor css fixes on popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,14 +2,14 @@
 <v-flex xs12>
   <table  rules="rows">
     <tr>
-      <th>Date</th>
-      <th>Description</th>
+      <th style="text-align:left;" class="pa-2">Date</th>
+      <th style="text-align:left;" class="pa-2">Description</th>
       <th></th>
     </tr>
     <tr v-for="net in networksData" :key="net.name">
       <td><p class="pr-2" style="font-weight:bold;">{{ net.date | moment }}</p></td>
       <td style="max-width:200px;"><p class="pa-2" style="font-style:italic;">{{ net.description }}</p></td>
-      <td><v-btn small depressed outline color="primary" @click="$emit('updateNet', net.id)" style="width:50px;font-size:11px;margin:0px;text-transform: none !important;">View</v-btn></td>
+      <td><v-btn small depressed color="teal" dark @click="$emit('updateNet', net.id)" style="width:50px;font-size:11px;margin:0px;text-transform: none !important;">View</v-btn></td>
     </tr>
   </table>
 </v-flex>
@@ -28,10 +28,6 @@ export default {
 </script>
 
 <style scoped>
-table {
-    display: table;
-    border-color: #004e6b;
-}
 p {
   margin: 0;
 }
